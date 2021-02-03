30c7901201b8c27df8cde24d3958d47fb4abde98
Eduardo Gonzalez <eduardo.gonzalez.flores28@gmail.com>
2021-02-03 12:20:32 -0500

    cambios examen de recuperacion

--- examenRecuperacion.py
+++ examenRecuperacion.py
@@ -1,43 +1,114 @@
-# Programa para calcular el area de un triangulo conociendo solo sus vertices, se utiliza la formula de Herón
-# Ingresa los puntos del plano cartesiano
+"""
+Examen de Recuperación 3D
+Graficacion 
+ISIC
+EDUARDO GONZALEZ FLORES
+"""
 import matplotlib.pyplot as plt
 import numpy as np
-from math import *
+from math import sin, cos, radians, sqrt
 
-x1 = int(input("Ingrese X1: "))
-y1 = int(input("Ingrese Y1: "))
-x2 = int(input("Ingrese X2: "))
-y2 = int(input("Ingrese Y2: "))
-x3 = int(input("Ingrese X3: "))
-y3 = int(input("Ingrese Y3: "))
+plt.axis([0, 150, 100, 0])
+plt.axis('on')
+plt.grid(False)
+x = [40, 70, 80, 75]  # Aqui podemos cambiar las coordenadas de los puntos *obvio*
+y = [60, 10, 40, 60]
+z = [0, 0, 0, 0]
 
-#Calcula e imprime las longitudes de los lados utilizando la formula de distancia entre 2 puntos
-print("Los puntos son:(", x1, ",", y1, ")(", x2, ",", y2, ")(", x3, ",", y3, ")")
-a1 = (y2-y1)**2 + (x2-x1)**2
-a = sqrt(a1)
-b1 = (y3-y2)**2 + (x3-x2)**2
-b = sqrt(b1)
-c1 = (y3-y1)**2 + (x3-x1)**2
-c = sqrt(c1)
+plt.plot([x[0], x[1]], [y[0], y[1]], color='k')  # ——plot plane A
+plt.plot([x[1], x[2]], [y[1], y[2]], color='k')
+plt.plot([x[2], x[0]], [y[2], y[0]], color='k')
+plt.scatter(x[3], y[3], s=20, color='r')
+
+plt.plot([x[0], x[3]], [y[0], y[3]], linestyle=':', color='r')  # plot planes
+plt.plot([x[1], x[3]], [y[1], y[3]], linestyle=':', color='r')
+plt.plot([x[2], x[3]], [y[2], y[3]], linestyle=':', color='r')
+plt.text(35, 63, '0')  # ——label corners
+plt.text(25, 10, '1')
+plt.text(83, 63, '2')
+plt.text(x[3]+2, y[3], '3')
+
+a = x[1]-x[0]  # calculamos las vertices
+b = y[1]-y[0]
+c = z[1]-z[0]
+Q01 = sqrt(a*a+b*b+c*c)
+
+a = x[2]-x[1]
+b = y[2]-y[1]
+c = z[2]-z[1]
+Q12 = sqrt(a*a+b*b+c*c)
+
+a = x[2]-x[0]
+b = y[2]-y[0]
+c = z[2]-z[0]
+Q02 = sqrt(a*a+b*b+c*c)
+
+a = x[1]-x[3]
+b = y[1]-y[3]
+c = z[1] = z[3]
+Q13 = sqrt(a*a+b*b+c*c)
+
+a = x[2]-x[3]
+b = y[2]-y[3]
+c = z[2]-z[3]
+Q23 = sqrt(a*a+b*b+c*c)
+
+a = x[0]-x[3]
+b = y[0]-y[3]
+c = z[0]-z[3]
+Q03 = sqrt(a*a+b*b+c*c)
+
+s = (Q01+Q12+Q02)/2  # ——calculate areas A, A1 and A2
+A = sqrt(s*(s-Q01)*(s-Q12)*(s-Q02))
+
+s1 = (Q01+Q03+Q13)/2
+A1 = sqrt(s1*(s1-Q01)*(s1-Q03)*(s1-Q13))
+
+s2 = (Q02+Q23+Q03)/2
+A2 = sqrt(s2*(s2-Q02)*(s2-Q23)*(s2-Q03))
 
 
-print("La longitud del lado a es:", a)
-print("La longitud del lado b es:", b)
-print("La longitud del lado c es:", c)
+plt.arrow(70, 55, 10, 15, linewidth=.5, color='grey')  # ——label area A
+plt.text(82, 73, 'A', color='k')
+plt.text(100, 40, 'A=')  # ——plot output
+dle = '%7.0f' % (A)
+dls = str(dle)
+plt.text(105, 40, dls)
 
-#Segun la formula de Herón para hallar el area de un triangulo conociendo solo sus lados es necesario el semiperimetro"
+plt.text(100, 45, 'A1=', color='r')
+dle = '%7.0f' % (A1)
+dls = str(dle)
+plt.text(105, 45, dls)
+plt.text(100, 50, 'A2=', color='r')
+dle = '%7.0f' % (A2)
+dls = str(dle)
+plt.text(105, 50, dls)
+plt.text(91, 55, 'A1+A2=', color='r')
+dle = '%7.0f' % (A1+A2)
+dls = str(dle)
+plt.text(106, 55, dls)
+plt.text(100, 40, 'A=')
+dle = '%7.0f' % (A)
+dls = str(dle)
+plt.text(105, 40, dls)
+plt.text(100, 45, 'A1=', color='r')
+dle = '%7.0f' % (A1)
+dls = str(dle)
+plt.text(105, 45, dls)
 
-p = (a+b+c)/2.0
+plt.text(100, 50, 'A2=', color='r')
+dle = '%7.0f' % (A2)
+dls = str(dle)
+plt.text(105, 50, dls)
 
-print("El semiperimetro es:", p)
+plt.text(91, 55, 'A1+A2=', color='r')
+dle = "%7.0f" % (A1+A2)
+dls = str(dle)
+plt.text(106, 55, dls)
 
-#Calculando el area
+if A1+A2 > A:
+    plt.text(90, 63, 'FUERA DEL HOYO', color='PURPLE')
+else:
+    plt.text(90, 63, 'DENTRO DEL HOYO', color='PURPLE')
 
-A1 = p*(p-a)*(p-b)*(p-c)
-A = sqrt(A1)
-
-
-print("El Area del tringulo es:", A)
-
-
-#Fin
+plt.show()
